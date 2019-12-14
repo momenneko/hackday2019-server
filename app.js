d63fd1c0ea4b8934ed669f3a16d5de008fe21762
momenneko <momenneko23@gmail.com>
2019-12-14 16:11:52 +0900

    package.json修正など

--- app.js
+++ app.js
@@ -3,25 +3,31 @@ let express = require('express');
 let fs = require("fs");
 let request = require('request');
 let face = require('./faceapi.js');
-require('dotenv').config();
+let env = require('dotenv').config();
 
 const app = express();
 app.use(express.json({ extended: true, limit: '10mb' })); // サイズ上限を10MBに拡張
 
 app.post('/', (req, res) => {
-    const bodyjson = req.body; 
+    const bodyjson = req.body.data; 
     const username = bodyjson.name;
     const twitter_id = bodyjson.twitter_id;
     const github_id = bodyjson.github_id;
     const face_image = bodyjson.face_image;
 
     console.log(username);
-    
+
     // faceAPIに問い合わせ
     let face_b64 = Buffer.from(face_image, 'base64');
-    // face.registerFace(face_b64);
+    let faceId = face.registerFace(face_b64);
     
     res.send("OK");
 });
+
+app.post('/detect', (req, res) => {
+    const bodyjson = req.body.data;
+    console.log(bodyjson);
+    res.send("ok");
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
--- faceapi.js
+++ faceapi.js
@@ -1,13 +1,35 @@
 let rp = require('request-promise');
+require('dotenv').config();
 
 // face API settings
 const subscriptionKey = process.env.SUBSCRIPTION_KEY;
-const uriBase = 'https://' + process.env.END_POINT + '.com/face/v1.0/detect';
+const personGroupId = 'hackday2019';
 
 async function registerFace(img_b64) {
-    var options = {
+    let faceId = detectFace(img_b64);
+}
+
+// 使わない
+function createPersonGroup(group_name) { 
+    let body = JSON.parse({"name": group_name}); 
+    let personGroupId = group_name + Date.now;
+    console.log(personGroupId);
+
+    const options = {
+        method: 'PUT',
+        uri: 'https://' + process.env.FACEAPI_END_POINT + `.com/face/v1.0/persongroups/${personGroupId}`,
+        body: body,
+        headers: {
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key' : subscriptionKey
+        }
+    };
+}
+
+async function detectFace(img_b64) {
+    const options = {
         method: 'POST',
-        uri: uriBase,
+        uri: 'https://' + process.env.FACEAPI_END_POINT + '.com/face/v1.0/detect',
         body: img_b64, // 生のbinaryを直接送る
         headers: {
             'Content-Type': 'application/octet-stream',
@@ -19,31 +41,66 @@ async function registerFace(img_b64) {
     try {    
         let body = await rp(options)
         let parsedBody = JSON.parse(body)
-        let faceId = parsedBody[0].faceId
+
+        let faceId = parsedBody[0] ? parsedBody[0].faceId : null
         
         console.log('JSON Response')
         console.log(parsedBody)
-        console.log(faceId)
-
+        
+        let personId = await identityFace([faceId], personGroupId) 
+        console.log(`faceId: ${faceId}`)
+        
         return faceId;
     } catch (err) {
         console.log(err)
         return;
-    }
+    }    
+}
+
+async function identityFace(faceIds, personGroupId) { 
+    const body = JSON.parse(
+        {
+            "personGroupId": personGroupId,
+            "faceIds": faceIds,
+            // "maxNumOfCandidatesReturned": 1,
+            // "confidenceThreshold": 0.5
+        }
+    );
+    console.log(body);
+    const options = {
+        method: 'POST',
+        uri: 'https://' + process.env.FACEAPI_END_POINT + '.com/face/v1.0/identify',
+        body: body,
+        headers: {
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key' : subscriptionKey
+        }
+    };
+    
+    // 同期処理にするためにawait
+    try {    
+        let body = await rp(options)
+        let parsedBody = JSON.parse(body)
+
+        // let faceId = parsedBody[0] ? parsedBody[0].faceId : null
+        
+        console.log('JSON Response')
+        console.log(parsedBody)
+        
+        // console.log(`faceId: ${faceId}`)
+        
+        return;
+    } catch (err) {
+        console.log(err)
+        return;
+    }    
 }
 
 let foo = () => {
     return 1;
 }
 
-function diag(x, y) {
-    return sqrt(square(x) + square(y));
-}
-
 module.exports={
    foo: foo,
-   registerFace: registerFace,
-   diag: diag
+   registerFace: registerFace
 };
-
-// module.exports.registerFace = registerFace
